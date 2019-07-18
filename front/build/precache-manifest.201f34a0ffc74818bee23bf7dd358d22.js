b72dc54f7dd4b1ccfbbca3f8ed5b0e18e3a0cc20
LeonardoAvendaño <leavendanoro@unal.edu.co>
2019-07-17 23:15:22 -0500

    front tiene la version dockerizada de la version de produccion de sa_academy_fe.

---
+++ front/build/precache-manifest.201f34a0ffc74818bee23bf7dd358d22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169c9c72f299a214f93c9e16c45225e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5bc8b02de2dbaa9f2e4",
+    "url": "/static/css/main.18192465.chunk.css"
+  },
+  {
+    "revision": "abfcf74894bb34c710bf",
+    "url": "/static/js/2.6dde4d4d.chunk.js"
+  },
+  {
+    "revision": "e5bc8b02de2dbaa9f2e4",
+    "url": "/static/js/main.230d7600.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
